e76d70dd5c8ca96b053f8e162a67a328ab14d308
Ansari Siraj <sirajrolln@gmail.com>
2025-08-03 23:39:57 +0530

    Add Lombok Dependency in pom.xml and use @Getter and @Setter annotation to avoid writing all the getters and setters in JournalEntry class

--- src/main/java/net/engineeringdigest/journalApp/Entity/JournalEntry.java
+++ src/main/java/net/engineeringdigest/journalApp/Entity/JournalEntry.java
@@ -1,11 +1,15 @@
 package net.engineeringdigest.journalApp.Entity;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 //this annotation tells the mongoDB that this POJO class will be mapped to a Document in the MongoDB database. The instance of the JournalEntry will be equal to the Document
 @Document(collection = "journal_entries")
+@Getter
+@Setter
 public class JournalEntry {
 
 //this annotation is used to denote the primary key in our collection.
@@ -14,27 +18,5 @@ public class JournalEntry {
     private String title;
     private String content;
 
-    public ObjectId getId() {
-        return id;
-    }
 
-    public String getTitle() {
-        return title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
